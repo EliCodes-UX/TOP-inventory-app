46bde2de4db6a4f62b8da789d9bbdbe37feb4efb
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-04 18:14:29 -0700

    added item and category routes

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var itemRouter = require('./routes/item');
+var categoryRouter = require('./routes/category');
 
 var app = express();
 
@@ -30,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/item', itemRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
